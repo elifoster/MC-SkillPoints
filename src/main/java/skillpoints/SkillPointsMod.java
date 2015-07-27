b3060a5a3bf652d518d9fa4d2ab5e3db4b661129
Striking <Strikingwolf2012@gmail.com>
2015-07-26 20:00:32 -0500

    Someone forgot an import
    Signed-off-by: Striking <Strikingwolf2012@gmail.com>

--- src/main/java/skillpoints/SkillPointsMod.java
+++ src/main/java/skillpoints/SkillPointsMod.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.common.MinecraftForge;
 import skillpoints.handler.*;
 
 @Mod(modid = SkillPointsMod.MODID, name = SkillPointsMod.NAME, version = SkillPointsMod.VERSION)
